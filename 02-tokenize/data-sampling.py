28f4bac51673899d7780ea52a92697a6a96a5f63
hyorch <hyorch@gmail.com>
2025-09-07 18:03:14 +0200

    data sampling

--- 02-tokenize/data-sampling.py
+++ 02-tokenize/data-sampling.py
@@ -15,3 +15,18 @@ print(len(enc_text))
 
 enc_sample = enc_text[50:]
 
+context_size = 4
+x = enc_sample[:context_size]
+y = enc_sample[1:context_size+1]
+print(f"x: {x}")
+print(f"y:      {y}")
+
+for i in range(1, context_size+1):
+    context = enc_sample[:i]
+    desired = enc_sample[i]
+    print(context, "---->", desired)
+
+for i in range(1, context_size+1):
+    context = enc_sample[:i]
+    desired = enc_sample[i]
+    print(tokenizer.decode(context), "---->", tokenizer.decode([desired]))
